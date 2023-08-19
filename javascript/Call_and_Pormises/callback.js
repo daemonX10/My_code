fe2b5c3b17ca94d41873fc6cf337b625f8499843
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-08-19 07:31:02 +0530

    callback

--- javascript/Call_and_Pormises/callback.js
+++ javascript/Call_and_Pormises/callback.js
@@ -1,1 +1,14 @@
 // # callback is a function that is pass&; as an argument to another function an; is &x&cut&; onc& th& main function has finish&; executing.
+
+// Function to find square of a number
+
+function squareOfANumber(num){
+    console.log(num*num);
+}
+function operation(num,fun){
+    fun(num);
+}
+
+operation(3,squareOfANumber);
+
+// Synchronous Operations
