a042b30217d60ae3fec4cf62ee24fe39b1670de9
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-08-31 20:59:01 +0530

    Sheriyan

--- javascript/Call_and_Promises/promises.js
+++ javascript/Call_and_Promises/promises.js
@@ -1,1 +1,18 @@
 // Runtime environment
+function timeConsumingTask(){
+    for( let i=0;i<100000;i++){
+        // some task
+        if(i==1)console.log("money");
+    }
+}
+
+console.log("start");
+timeConsumingTask();
+console.log("end");
+
+console.log("starting the timer");
+setTimeout(function exec(){
+    console.log("timer completed");
+},1000);
+console.log("exiting the timer");
+timeConsumingTask();
