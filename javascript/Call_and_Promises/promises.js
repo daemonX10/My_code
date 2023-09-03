9bf248f0ce62a414db371374dac4b2d61b2e149f
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-09-03 12:37:38 +0530

    promises

--- javascript/Call_and_Promises/promises.js
+++ javascript/Call_and_Promises/promises.js
@@ -1,18 +1,33 @@
-// Runtime environment
+// // Runtime environment
+// function timeConsumingTask(){
+//     for( let i=0;i<100000;i++){
+//         // some task
+//         if(i==1)console.log("money");
+//     }
+// }
+
+// console.log("start");
+// timeConsumingTask();
+// console.log("end");
+
+// console.log("starting the timer");
+// setTimeout(function exec(){
+//     console.log("timer completed");
+// },1000);
+// console.log("exiting the timer");
+// timeConsumingTask();
+
 function timeConsumingTask(){
-    for( let i=0;i<100000;i++){
-        // some task
-        if(i==1)console.log("money");
+    for(let i=0;i<10000000;i++){
+        // code
     }
 }
 
 console.log("start");
 timeConsumingTask();
-console.log("end");
+console.log("start the timer");
+setTimeout(function exec(){//  this will wait for 10sec than print it output , not in between disturbance
+    console.log("timer completed")
+},10000);
 
-console.log("starting the timer");
-setTimeout(function exec(){
-    console.log("timer completed");
-},1000);
-console.log("exiting the timer");
-timeConsumingTask();
+console.log("end");
