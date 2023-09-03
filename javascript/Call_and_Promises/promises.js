48eaee808aa03754bc425c46d3f4207a4dd78d55
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-09-03 21:28:11 +0530

    promises

--- javascript/Call_and_Promises/promises.js
+++ javascript/Call_and_Promises/promises.js
@@ -17,17 +17,49 @@
 // console.log("exiting the timer");
 // timeConsumingTask();
 
-function timeConsumingTask(){
-    for(let i=0;i<10000000;i++){
-        // code
-    }
+// function timeConsumingTask(){
+//     for(let i=0;i<10000000;i++){
+//         // code
+//     }
+// }
+
+// console.log("start");
+// timeConsumingTask();
+// console.log("start the timer");
+// setTimeout(function exec(){//  this will wait for 10sec than print it output , not in between disturbance
+//     console.log("timer completed")
+// },10000);
+
+// console.log("end");
+
+function returnDummyPromise(){
+    return new Promise( function(resolve,reject){
+        // write your code here
+        // let randomNumber=Math.random();
+        //     console.log(randomNumber);
+        //     if(randomNumber>0.5){
+        //         resolve("this promise is resolved . The number is greater than 0.5");
+        //     }
+        //     else{
+        //         reject("The promise is rejected . The number is lesser than 0.5");
+        //     }
+    
+        for(let i=0;i<1000000000;i++){
+            // code
+            if(i==100000000){
+                resolve("this promise is resolved ");
+            }
+        }
+        
+    })
 }
 
-console.log("start");
-timeConsumingTask();
-console.log("start the timer");
-setTimeout(function exec(){//  this will wait for 10sec than print it output , not in between disturbance
-    console.log("timer completed")
-},10000);
-
+let p=returnDummyPromise();
+p
+.then(function executeOnResolve(data){
+    console.log(data);
+})
+.catch(function executeOnReject(err){
+    console.log(err);
+})
 console.log("end");
