9b4899feb4fbd98fc79d34e13d726f7f0cb50527
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-09-10 00:23:24 +0530

    my first sql connection

--- mysql/films_database.sql
+++ mysql/films_database.sql
@@ -1,1 +1,22 @@
-SHOW DATABASES;
+CREATE DATABASE IF NOT EXISTS films_database;
+use  films_database;
+
+CREATE TABLE IF NOT EXISTS movies(
+    m_name VARCHAR(100)  not NULL,
+    rating INTEGER not NULL
+);
+desc movies;
+
+-- insert data in table 
+INSERT INTO movies (rating , m_name)
+VALUES
+(4,"avenger");
+
+INSERT INTO movies (rating , m_name)
+VALUES
+(3,"thor love and thunder"),
+(3 , "thor the dark world"),
+(5,"avenger infinity war"),
+(4,"Iron man ");
+
+SELECT * FROM movies;
