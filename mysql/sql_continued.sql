f326564d894e447af4db3850fc50b0962e1096da
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-09-11 01:31:43 +0530

    advance sql

---
+++ mysql/sql_continued.sql
@@ -0,0 +1,26 @@
+-- Active: 1694119625501@@127.0.0.1@3306@lco_films
+SHOW TABLES;
+DESCRIBE film;
+
+ALTER TABLE film  DROP length;
+ALTER Table film ADD length SMALLINT NOT NULL ;
+ALTER TABLE film MODIFY length int UNSIGNED;
+
+SHOW TABLES;
+SELECT DISTINCT district FROM address;
+
+SELECT COUNT(*) FROM address;
+
+SELECT COUNT(DISTINCT district )FROM address;
+
+SELECT * FROM film;
+
+SELECT * FROM film WHERE title LIKE 'A%';
+
+SELECT MAX(rental_duration) FROM film;
+
+SELECT * FROM film WHERE rental_duration=7;
+
+SELECT AVG(rental_duration) FROM film;
+SELECT MIN(rental_duration) FROM film;
+SELECT SUM(rental_duration) FROM film;
