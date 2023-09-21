044122554314baadcbcd517f1f9b7c48ed1db9f2
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-09-21 22:50:19 +0530

    course collection

--- javascript/mongoDB/playground-2.mongodb.js
+++ javascript/mongoDB/playground-2.mongodb.js
@@ -1,4 +1,7 @@
+
 db;
+
+db.getMongo().getDBs();
 
 db.getMongo().getDBs();
 
@@ -9,3 +12,47 @@ db.getCollectionNames();
 db.createCollection("course");
 
 db.course.find();
+
+db.course.insertOne({  // inserting data 
+    name:"Physics",
+    type:"practical",
+    marks:100
+});
+
+db.course.insertOne({
+    name:"chemistry",
+    type:"practical",
+    marks:100
+});
+
+db.course.find();
+
+
+use('new_db');
+db.createCollection("new_collection");
+db.new_collection.in({
+    name:"damdoar",
+    roll_no:50,
+    course:"computer science ",
+    college:"Thakur college",
+    Nick_name:"Golu"
+});
+
+/// student data now
+
+use("test");
+
+db.getCollectionNames();
+
+db.course.insertMany([{
+    name:"English",
+    type:"Theory"
+},{
+    name:"P.Ed",
+    mandatory:"NO"
+}
+]);
+
+db.course.find();
+
+
