f8967fa44bc12dee0e7a40d4bf77e3fe77424b05
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-11-30 01:23:26 +0530

    Refactor forEach and map functions

--- javascript/HOF and function programming/forEach_map_filter.js
+++ javascript/HOF and function programming/forEach_map_filter.js
@@ -8,7 +8,37 @@
 let itemsInCart =["apple","comb","mike","keyboard","t-shirt","mobile holder"];
 // Display the item in cart 
 // itemsInCart.forEach((item)=>console.log(item));
-itemsInCart.forEach((item,index,Array) => 
-console.log(
-    
-))
+// itemsInCart.forEach((element,index,arr)=>{
+//     console.log(`${index} and their element ${element}`)
+// });
+
+// console.log(itemsInCart);
+
+// const filteredItems = itemsInCart.filter((element,index,arr)=>{
+//     return element.length>4
+// })
+// console.log(filteredItems);
+
+// const filteredItems2 = itemsInCart.filter(element=>element.includes("e"));
+// console.log(filteredItems2);
+
+// const filteredItems3 = itemsInCart.filter((element,index)=>{
+//     console.log(element,index);
+//     return element.toUpperCase().includes("e");
+// })
+
+// console.log(filteredItems3);
+
+// map
+// map is used to create a new array from an existing array
+// map is used to modify the existing array
+
+const array = [1,2,3,4,5];
+
+// array.map((element,index,arr)=>{
+//     console.log(element,index,arr);
+// })
+
+array.map((el)=>{
+    console.log(el);
+})
